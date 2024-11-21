45349319b8ef2be19cd08b52276e573efefbc807
Flavia Martinez <flavia.martinez@uc.cl>
2024-11-20 21:32:31 -0300

    Add functionality to buttons for incrementing and decrementing quantity

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,30 @@
-precio = 400000
+const precio = 400000
+let cantidad = 0
+let precio_total = 0
 
-precioSpan = document.querySelector(".precio-inicial");
+const precioSpan = document.querySelector(".precio-inicial")
 precioSpan.innerHTML = precio
+
+const cantidadSpan = document.querySelector(".cantidad")
+cantidadSpan.innerHTML = cantidad
+
+const precioTotalSpan = document.querySelector(".valor-total")
+
+const btnSum = document.querySelector("#btn-sum")
+const btnSub = document.querySelector("#btn-sub")
+
+btnSum.addEventListener('click', () => {
+	cantidad += 1;
+	cantidadSpan.innerHTML = cantidad;
+	precio_total = cantidad * precio;
+	precioTotalSpan.innerHTML = precio_total
+})
+
+btnSub.addEventListener('click', () => {
+	if (cantidad > 0) {
+		cantidad -= 1;
+		cantidadSpan.innerHTML = cantidad;
+		precio_total = cantidad * precio;
+		precioTotalSpan.innerHTML = precio_total
+	}
+})
